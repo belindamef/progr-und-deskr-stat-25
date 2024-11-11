29d9579e32a005bf19461df101a261453bde76d4
Belinda Fleischmann <fl.belinda@googlemail.com>
2024-11-11 18:09:21 +0100

    Add (5)

---
+++ 5_Listen_und_Dataframes/table_viewer_Demo.r
@@ -0,0 +1,14 @@
+# Skript zur Demonstration des VSCode Table Viewers
+# ----------------------
+# Datum 18-09-2024
+# Belinda Fleischmann
+
+# Erzeugen eines Dataframes
+D <- data.frame(
+  x = letters[1:4],                  # 1. Spalte mit Name x
+  y = 1:4,                           # 2. Spalte mit Name y
+  z = c(TRUE, TRUE, FALSE, TRUE)     # 3. Spalte mit Name z
+)
+
+# Ausgabe des Dataframes in der Console
+print(D)
